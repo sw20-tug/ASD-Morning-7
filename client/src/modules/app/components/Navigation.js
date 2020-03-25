92aa4d056dd57419399eb3803bfc20296c033c76
Sebatian Puck <info@pucks.me>
2020-03-25 10:05:28 +0100

    added navigation component

---
+++ client/src/modules/app/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+
+    render () {
+        return (
+            <h1>Navigation</h1>
+        );
+    }
+}
+
+export default Navigation;
