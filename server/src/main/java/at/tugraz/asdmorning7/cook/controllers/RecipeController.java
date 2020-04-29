1368bafd257f8911ea92ca66a8ab9c7fc1a13067
Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>
2020-04-29 11:01:38 +0200

    COOK-Hotfix Fix CORS error [PD]
    Signed-off-by: Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>

--- server/src/main/java/at/tugraz/asdmorning7/cook/controllers/RecipeController.java
+++ server/src/main/java/at/tugraz/asdmorning7/cook/controllers/RecipeController.java
@@ -6,6 +6,7 @@ import at.tugraz.asdmorning7.cook.exceptions.RecipeNotFoundException;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/recipes")
 public class RecipeController {
